dc2c75df9f44b28e2c386fa26a24d9cdc71ece54
Kien <giakien0401@gmail.com>
2025-03-04 23:40:55 +0700

    updating route & controller

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const app = express();
 const port = 3000;
 const handlebars = require('express-handlebars');
 const morgan = require('morgan');
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, "public")));
 
+
+//Body-parser
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
@@ -16,12 +23,7 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, "resources", "views"));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Routes init 
+route(app);
 
 app.listen(port, ()=> console.log(`Listening at http://localhost:3000`));
