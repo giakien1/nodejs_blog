7e716f4605d685ff37dbf1a8dfdd11f39632b32e
Kien <giakien0401@gmail.com>
2025-03-05 14:34:05 +0700

    install prettier, lint-staged husky

--- src/index.js
+++ src/index.js
@@ -5,25 +5,24 @@ const port = 3000;
 const handlebars = require('express-handlebars');
 const morgan = require('morgan');
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
-
+    app.use(express.static(path.join(__dirname, 'public')));
 
 //Body-parser
-app.use(express.urlencoded({extended : true}));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
 //HTTP logger
 // app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+        app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
-app.set('views', path.join(__dirname, "resources", "views"));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-//Routes init 
+//Routes init
 route(app);
 
-app.listen(port, ()=> console.log(`Listening at http://localhost:3000`));
+app.listen(port, () => console.log(`Listening at http://localhost:3000`));
